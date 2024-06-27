5ed132e17307f1990f25c0f533d5f5aae0f2a878
Marvin <m.schuerz@hautzy.com>
2024-06-27 21:30:43 +0200

    Add delta property to drag event

--- src/input/events/DragEvent.ts
+++ src/input/events/DragEvent.ts
@@ -1,3 +1,4 @@
+import type { Drawable } from "../../graphics/drawables/Drawable";
 import type { Vec2 } from "../../math";
 import type { InputState } from "../state/InputState";
 import type { MouseButton } from "../state/MouseButton";
@@ -16,4 +17,22 @@ export class DragEvent extends UIEvent {
   }
 
   readonly screenSpaceLastMousePosition: Vec2;
+
+  get screenSpaceDelta(): Vec2 {
+    return this.screenSpaceMousePosition.sub(this.screenSpaceLastMousePosition);
+  }
+
+  localSpaceDelta(drawable: Drawable): Vec2 {
+    const position = drawable.toLocalSpace(this.screenSpaceMousePosition);
+    const lastPosition = drawable.toLocalSpace(this.screenSpaceLastMousePosition);
+
+    return position.sub(lastPosition);
+  }
+
+  parentSpaceDelta(drawable: Drawable): Vec2 {
+    const position = drawable.parent!.toLocalSpace(this.screenSpaceMousePosition);
+    const lastPosition = drawable.parent!.toLocalSpace(this.screenSpaceLastMousePosition);
+
+    return position.sub(lastPosition);
+  }
 }
