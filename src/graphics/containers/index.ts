70599c4ad5204d7c321c4d896a53aab3b38f1d8b
Marvin Schürz <minetoblend@gmail.com>
2024-06-30 00:12:36 +0200

    Add export for ButtonTrigger

--- src/graphics/containers/index.ts
+++ src/graphics/containers/index.ts
@@ -1,6 +1,6 @@
 export { CompositeDrawable, type CompositeDrawableOptions } from './CompositeDrawable';
 export { Container, type ContainerOptions } from './Container';
 export { DrawSizePreservingFillContainer } from './DrawSizePreservingFillContainer';
-export { ClickableContainer } from './ClickableContainer';
+export { ClickableContainer, ButtonTrigger } from './ClickableContainer';
 export { FlowContainer } from './FlowContainer';
 export { FillFlowContainer, FillDirection } from './FillFlowContainer';
