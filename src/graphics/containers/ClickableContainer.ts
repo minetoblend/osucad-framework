18d2e187a5814a18df2fd1ce073985bd8d911208
Marvin Schürz <minetoblend@gmail.com>
2024-06-29 22:06:58 +0200

    Add trigger option to clickableContainer

--- src/graphics/containers/ClickableContainer.ts
+++ src/graphics/containers/ClickableContainer.ts
@@ -1,4 +1,5 @@
 import { Bindable } from "../../bindables/Bindable";
+import type { MouseDownEvent } from "../../input";
 import type { ClickEvent } from "../../input/events/ClickEvent";
 import { Container } from "./Container";
 
@@ -14,11 +15,24 @@ export class ClickableContainer extends Container {
     if(value) this.enabled.value = true;
   }
 
+  public trigger = ButtonTrigger.Click;
+
   public readonly enabled = new Bindable(false);
 
   override onClick(e: ClickEvent): boolean {
-    if (this.enabled.value) this.action?.();
+    if (this.enabled.value && this.trigger === ButtonTrigger.Click) this.action?.();
     return true;
   }
+  
+  override onMouseDown(e: MouseDownEvent): boolean {
+    if (this.enabled.value && this.trigger === ButtonTrigger.MouseDown) this.action?.();
+    return true;
+  
+  }
 
+}
+
+export enum ButtonTrigger {
+  Click,
+  MouseDown,
 }
