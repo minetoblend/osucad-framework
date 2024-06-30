731363616a8a64573703bd1f368de3706fc89d60
Marvin <m.schuerz@hautzy.com>
2024-06-30 21:51:17 +0200

    add export for MenuItem class

--- src/graphics/userInterface/index.ts
+++ src/graphics/userInterface/index.ts
@@ -1,2 +1,5 @@
 export { Button } from './Button';
 export { BasicButton, type BasicButtonOptions } from './BasicButton';
+export { Menu, DrawableMenuItem, MenuItemState, MenuState } from './Menu';
+export { BasicMenu } from './BasicMenu';
+export { MenuItem, type MenuItemOptions } from './MenuItem';
