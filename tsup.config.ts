c2ff3e0054a1143ea47b0d64b4877baec29c4a9d
Marvin Schürz <minetoblend@gmail.com>
2024-07-14 23:22:15 +0200

    Add cjs build target

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  target: "esnext",
+  target: "es2020",
   dts: true,
-  format: ["esm"],
+  format: ["esm", 'cjs'],
   sourcemap: true,
   clean: true,
   outDir: "dist",
